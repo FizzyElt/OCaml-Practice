3844fd84ae2e8de129ef7f7ca99c8c0dad4c729a
FizzyElt <fizzyelt8786@gmail.com>
2023-09-10 23:43:27 +0800

    init ocaml

---
+++ beginner/list.ml
@@ -0,0 +1,85 @@
+(* Tail fo List *)
+let rec last = function [] -> None | x :: [] -> Some x | _ :: xs -> last xs
+
+(* Last Two Elements of a List *)
+let rec last_two = function
+  | [] | [ _ ] -> None
+  | [ x; y ] -> Some (x, y)
+  | _ :: xs -> last_two xs
+
+(* N'th Element of a List *)
+let rec at k = function
+  | [] -> None
+  | x :: xs -> if k = 0 then Some x else at (k - 1) xs
+
+(* Length of a List *)
+let length list =
+  let rec aux n = function [] -> n | _ :: xs -> aux (n + 1) xs in
+  aux 0 list
+
+(* Reverse a List *)
+let rev list =
+  let rec aux acc = function [] -> acc | x :: xs -> aux (x :: acc) xs in
+  aux [] list
+
+(* Palindrome *)
+let is_palindrome list = list = List.rev list
+
+type 'a node = One of 'a | Many of 'a node list
+
+let flatten list =
+  let rec aux acc = function
+    | [] -> acc
+    | One x :: t -> aux (x :: acc) t
+    | Many l :: t -> aux (aux acc l) t
+  in
+  aux [] list
+
+let encode list =
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> (count + 1, x) :: acc
+    | x :: y :: t ->
+        if x = y then aux (count + 1) acc t else aux 0 ((count + 1, x) :: acc) t
+  in
+  List.rev (aux 0 [] list)
+
+type 'a rle = One of 'a | Many of int * 'a
+
+let encode2 list =
+  let to_rle count x = if count = 1 then One x else Many (count, x) in
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> to_rle (count + 1) x :: acc
+    | x :: y :: t ->
+        if x = y then aux (count + 1) acc t
+        else aux 0 (to_rle (count + 1) x :: acc) t
+  in
+  List.rev (aux 0 [] list)
+
+let rec duplicate = function [] -> [] | x :: xs -> x :: x :: duplicate xs
+
+let split list n =
+  let rec aux n acc = function
+    | [] -> (List.rev acc, [])
+    | h :: t as l ->
+        if n = 0 then (List.rev acc, l) else aux (n - 1) (h :: acc) t
+  in
+  aux n [] list
+
+let remove_at n list =
+  let rec aux i acc = function
+    | [] -> acc
+    | x :: xs ->
+        if i + 1 = n then aux (i + 1) acc xs else aux (i + 1) (x :: acc) xs
+  in
+  aux 0 [] list
+
+let rec insert_at x n = function
+  | [] -> [ x ]
+  | h :: t as l -> if n = 0 then x :: l else h :: insert_at x (n - 1) t
+
+let range a b =
+  let rec aux a b = if a > b then [] else a :: aux (a + 1) b in
+
+  if a > b then List.rev (aux b a) else aux a b
